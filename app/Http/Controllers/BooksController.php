2eed28a4326a366343ac33724897817a05e6e5e6
Prasiddhi Bhattarai <prasiddhibhattarai@gmail.com>
2021-12-23 10:43:18 +0545

    added bookcontroller.php

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    public function index()
+    {
+        $data['books'] = Company::orderBy('id','desc')->paginate(5);
+        return view('books.index', $data);
+    }
+
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'book_name' => 'required',
+            'author_first_name' => 'required',
+            'author_second_name' => 'required',
+            'price' => 'required',
+            'total_pages'=>'required',
+        ]);
+        $book = new Book;
+        $book->book_name = $request->book_name;
+        $book->author_first_name = $request->author_first_name;
+        $book->author_second_name = $request->author_second_name;
+        $book->price = $request->price;
+        $book->total_pages = $request->total_pages;
+        $book->save();
+        return redirect()->route('books.index')
+        ->with('success','Book has been stored successfully.');
+    }
+    public function show(Book $book)
+    {
+        return view('books.show',compact('book'));
+    }
+    public function edit(Book $book)
+    {
+        return view('books.edit',compact('book'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'book_name' => 'required',
+            'author_first_name' => 'required',
+            'author_second_name' => 'required',
+            'price' => 'required',
+            'total_pages'=>'required',
+        ]);
+        $book = Book::find($id);
+        $book->book_name = $request->book_name;
+        $book->author_first_name = $request->author_first_name;
+        $book->author_second_name = $request->author_second_name;
+        $book->price = $request->price;
+        $book->total_pages = $request->total_pages;
+        $book->save();
+        return redirect()->route('books.index')
+        ->with('success','Book has been updated successfully');
+    }
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect()->route('books.index')
+        ->with('success','Book has been deleted successfully');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('books', BooksController::class);
