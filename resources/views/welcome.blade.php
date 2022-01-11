5a469f0a8050d010c2a8aa320438a89ab40cbbe2
Prasiddhi Bhattarai <prasiddhibhattarai@gmail.com>
2022-01-11 21:09:08 +0545

    Added slider images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,6 +32,28 @@
         </div>
     </nav>
 
+    <div id="carouselControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="" style="background-color: red;" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
     <div class="container mt-5">
         <div class="row">
             <h2>Products</h2>
